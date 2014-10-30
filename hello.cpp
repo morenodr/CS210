ac23a146949675d7d6a79b150049d895cb78bede
Rigoberto Moreno Delgado <rmoreno@cwolf.cs.sonoma.edu>
2014-10-29 19:18:24 -0700

    using argv

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char*argv[])
 {
-
-        cout << "Hello, World" << endl;
+        // Takes in argument vector  
+        cout << "Hello, " << argv[10] << endl;
 
 }
