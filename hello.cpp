a34db6b9d75296dcdcc306761e5f9734b8f5b787
Rigoberto Moreno Delgado <rmoreno@cwolf.cs.sonoma.edu>
2014-10-30 09:36:08 -0700

    using argv

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,25 @@
-#include <iostream>
+// Author: Rigoberto Moreno Delgado
+// CS 210 - Professor Paris
+// Project 4
 
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char*argv[])
 {
-        // Takes in argument vector  
-        cout << "Hello, " << argv[10] << endl;
-
+        string name = "World";
+        // Default
+	if (argv[10] == " ")
+	{  
+	    cout << "Hello, World" << endl;
+        }
+	// With argument
+	else
+	{
+	    name = argv[11];
+	    name.erase (0 ,5);
+	    cout << "Hello, " << name << endl;
+        }
+	
 }
